473a500bce684de03acf5f410b36b929404edebc
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-03-31 21:32:24 +0000

    added dbContext

---
+++ ac_web_test_api/AppDbContext/ApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ac_web_test_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ac_web_test_api.AppDbContext
+{
+    public class ApiDbContext:DbContext
+    {
+        public ApiDbContext()
+        {
+        }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> customer { get; set; }
+        public DbSet<Employee> employee { get; set; }
+        public DbSet<Invoice> invoice { get; set; }
+        public DbSet<Company> company { get; set; }
+    }
+}
