ec673c1c18b26abd4f9948708c321c79bef6e16b
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-03-31 21:34:07 +0000

    migrations

---
+++ ac_web_test_api/Migrations/20220328163351_InitialCreate.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ac_web_test_api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "company",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_company", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    corpName = table.Column<string>(nullable: true),
+                    address = table.Column<string>(nullable: true),
+                    companyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_customer_company_companyId",
+                        column: x => x.companyId,
+                        principalTable: "company",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employee",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    firstName = table.Column<string>(nullable: true),
+                    lastName = table.Column<string>(nullable: true),
+                    age = table.Column<int>(nullable: false),
+                    companyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employee", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_employee_company_companyId",
+                        column: x => x.companyId,
+                        principalTable: "company",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "invoice",
+                columns: table => new
+                {
+                    invNumber = table.Column<string>(nullable: false),
+                    invSum = table.Column<int>(nullable: false),
+                    currency = table.Column<string>(nullable: true),
+                    corpName = table.Column<string>(nullable: true),
+                    companyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_invoice", x => x.invNumber);
+                    table.ForeignKey(
+                        name: "FK_invoice_company_companyId",
+                        column: x => x.companyId,
+                        principalTable: "company",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_companyId",
+                table: "customer",
+                column: "companyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employee_companyId",
+                table: "employee",
+                column: "companyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_invoice_companyId",
+                table: "invoice",
+                column: "companyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer");
+
+            migrationBuilder.DropTable(
+                name: "employee");
+
+            migrationBuilder.DropTable(
+                name: "invoice");
+
+            migrationBuilder.DropTable(
+                name: "company");
+        }
+    }
+}
