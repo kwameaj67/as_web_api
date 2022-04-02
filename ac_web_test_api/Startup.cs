5faddb493718ac7ea9acda992ba7764bbe36c15d
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-04-02 19:51:44 +0000

    added swagger to api

--- ac_web_test_api/Startup.cs
+++ ac_web_test_api/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 namespace ac_web_test_api
 {
@@ -46,19 +47,30 @@ namespace ac_web_test_api
             // connectionStrings
             string  connectionString = Configuration.GetConnectionString("default");
             services.AddDbContext<ApiDbContext>(options => options.UseNpgsql(connectionString));
-
             // add services
             services.AddScoped<ICustomer, CustomerService>();
             services.AddScoped<IEmployee, EmployeeService>();
             services.AddScoped<IInvoice, InvoiceService>();
             services.AddScoped<ICompany, CompanyService>();
 
+            services.AddSwaggerGen( c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {   Title = "AS_WEB_TEST",
+                    Version = "v1",
+                    Description = "API for AS TEST"
+                });
+            });
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
             // use cors
             app.UseCors(options =>
             {
@@ -67,12 +79,12 @@ namespace ac_web_test_api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+               
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
             app.UseAuthorization();
             app.UseAuthentication();
 
